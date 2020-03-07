ef4190d0117df0a5b2777a97985470877392e588
KULLEP <malyukow.maksim@mail.ru>
2020-03-08 01:40:14 +0200

    1

--- vk_api_happy/main.js
+++ vk_api_happy/main.js
@@ -1,26 +1,76 @@
-$.ajax({
-	url: 'https://api.vk.com/method/groups.get?count=3&access_token=cb6a9ab3a5be20e329d7a60255b71be1414c6287934805a6dfe6d976820edd989da46d43dcd4d32122c0d&v=5.103',
-	method: 'GET',
-	dataType: 'JSONP',
-	success: function (data) {
+
+// https://oauth.vk.com/authorize?client_id=7348710&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups&response_type=token&v=5.103
+
+// https://api.vk.com/method/users.get?user_id=210700286&v=5.52
 
 
-console.log(data);
-let g = data.response.items.join(',');
-		console.log(g);
+
+
+
+function GetUrl(method, params) {
+	if(!method) throw new new Error('Нет метода');
+	params = params || {}; // Либо равны либо пустой объект
+
+	params['access_token'] = '13fd0eb705cecb91251a6f3c3a76656d2988b1780fb74a6148b573af00a08be166d14d37094645f59ab3c';
+
+	return `https://api.vk.com/method/${method}?${$.param(params)}&v=5.103`;
+}
+
+
+const sendRequest = (method, params, func) => {
+	$.ajax({
+		url: GetUrl(method, params),
+		method: 'GET',
+		dataType: 'JSONP',
+		success: func
+	});
+}
+
+
+const getIdGroupUser = () => {
+	sendRequest('groups.get', {count: 7}, function (data) {
+		drowGroupsOnLoad(data.response.items);
+	});
+}
+
+const drowGroupsOnLoad = (groups) => {
+
+
+
+	console.log(groups);
+
+	groups.forEach( function(e) {
+
 		$.ajax({
-			url: `https://api.vk.com/method/groups.getById?group_ids=${g}}&access_token=cb6a9ab3a5be20e329d7a60255b71be1414c6287934805a6dfe6d976820edd989da46d43dcd4d32122c0d&v=5.103`,
+			url: GetUrl('groups.getById', e),
 			method: 'GET',
 			dataType: 'JSONP',
 			success: function (d) {
 				console.log(d);
 			}
 		});
+	});
 
+	var html = '';
 
+	for (let i=0; i< groups.length; i++) {
+		let g = groups[i];
 
-
+		html += `
+		<li class="list-group-item disabled">
+		<a target="_blank" href=vk.com/id${groups['i']} >
+		<img src="" />
+		<span></span>	
+		</a>
+		</li>
+		`;
 	}
-});
+}
 
-console.log(3122222222222222222222222231);
+
+getIdGroupUser();
+
+
+
+
+
