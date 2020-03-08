d3c2daa2a0c282ce8e16047f8021f7820f58e975
KULLEP <malyukow.maksim@mail.ru>
2020-03-08 21:49:18 +0200

    Update main.js

--- vk_api_happy/main.js
+++ vk_api_happy/main.js
@@ -279,7 +279,7 @@ document.getElementById('search_group').onclick =() => {
 		for(let i=0; i<=data.response.items.length; i++) {
 			let d = data.response.items[i];
 			document.querySelector('#list_group_user').innerHTML +=`
-			<a class="nav-link" id='${d.id}' href='#' >
+			<a class="nav-link" id='${d.id}' href='#' onclick="btn_get_group_info($(this).attr('id'));">
 			<li class="list-group-item  ">
 			<img class="border border-secondary rounded-circle circle" src="${d.photo_50}" />
 			<span class="text-dark h6">${d.name}</span>	
