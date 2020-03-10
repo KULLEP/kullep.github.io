52d7733375ba005cbef5b0fb9788e50916207d52
KULLEP <malyukow.maksim@mail.ru>
2020-03-10 18:09:54 +0200

    Update main.js

--- vk_api_happy/main.js
+++ vk_api_happy/main.js
@@ -343,6 +343,14 @@ VK.api("wall.post", {
 	close_comments: form_post_happy_info.close_comments
 }, function (data) {		
 	console.log(data);
+	console.log({
+	owner_id: this_param_own_id,
+	friends_only: form_post_happy_info.friends_only,
+	from_group: form_post_happy_info.from_group,
+	message: form_post_happy_info.message,
+	publish_date: form_post_happy_info.publish_date,
+	close_comments: form_post_happy_info.close_comments
+});
 });
 }
 
