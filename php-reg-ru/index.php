d1cce1388c5a9b8a568b4658ee5abe4bb387fda8
KULLEP <malyukow.maksim@mail.ru>
2020-03-13 13:15:56 +0200

    1

--- php-reg-ru/index.php
+++ php-reg-ru/index.php
@@ -1,17 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-<body>
-	<script src="main.js"></script>
-</body>
-</html>
+<?
+
+function get_info_reg($id, $domain, $pass, $user_name)
+{
+
+// Преобразуем массив в URL-кодированную строку
+	$data_t = array( 	 
+		'domain_name' => $domain,
+		'service_id' => $id,
+		'input_format' => 'json',
+		'output_content_type' => 'plain',
+		'password' => $pass,
+		'username' => $user_name
+	);
+
+	$data = http_build_query($data_t);
+
+// Создаем параметры контекста
+	$options = array('http' => 
+		array(
+			'method'  => 'GET',
+			'header'  => 'Content-type: application/x-www-form-urlencoded',
+			'content' => $data
+		)
+	);
+
+$context  = stream_context_create($options); // Создаём контекст потока
+$result = file_get_contents('https://api.reg.ru/api/regru2/service/get_info', false, $context); // Отправляем запрос
+
+$r = json_decode($result);
+
+return($r->answer->services[0]->expiration_date);
+}
 
 
-
-
+// Параметры
+// 1 - id слуги
+// 2 - Домен
+// 3 - Пароль
+// 4 - Имя пользователя
+echo "Дата окончания услуги - ";
+echo (get_info_reg('1', 'test.ru', 'test', 'test'));
